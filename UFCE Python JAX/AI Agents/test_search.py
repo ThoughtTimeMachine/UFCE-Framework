9c6037f0b33bb9eebf4483a45e929f319388a26d
ThoughtTimeMachine <112581074+ThoughtTimeMachine@users.noreply.github.com>
2025-12-26 15:44:20 -0800

    Added info to files

--- UFCE Python JAX/AI Agents/test_search.py
+++ UFCE Python JAX/AI Agents/test_search.py
@@ -1,3 +1,24 @@
+# Copyright (C) 2025 Kyle Killian
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+
+# --- HARDWARE CONFIG (The Speed Cheat) ---
+# This must happen BEFORE importing JAX to lock the RAM (Pinned Memory).
+# It prevents JAX from gobbling all VRAM instantly, allowing for streaming.
+
 import numpy as np
 from sentence_transformers import SentenceTransformer
 
